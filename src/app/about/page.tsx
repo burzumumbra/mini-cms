0e5c94b898e7b4874323818811f5a7ae726132a5
ronald-flores-devbase <ronald@ronaldsequeira.com>
2023-05-15 21:40:55 -0600

    basic about and contact

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,7 +1,13 @@
+import { faker } from '@faker-js/faker';
+import { Title, Text } from "@tremor/react";
+
 export default function About() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      
+    <main className="flex min-h-screen flex-col items-center justify-between p-12">
+      <div className="flex flex-col gap-2">
+        <Title>{faker.lorem.lines(1)}</Title>
+        <Text className="max-w-[400px]">{faker.lorem.lines(24)}</Text>
+      </div>
     </main>
   )
 }
--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,7 +1,14 @@
+import { faker } from '@faker-js/faker';
+import { Title, Text } from "@tremor/react";
+
 export default function Contact() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      
+    <main className="flex flex-col items-center justify-between pt-12">
+      <Title>Contact us</Title>
+      <Text className="max-w-[400px]">{faker.company.name()}</Text>
+      <Text className="max-w-[400px]">{faker.location.streetAddress(true)}</Text>
+      <Text className="max-w-[400px]">{faker.location.city() }, {faker.location.state({ abbreviated: true })} {faker.location.zipCode('####')}</Text>
+      <Text className="max-w-[400px]">{faker.phone.number('+48 91 ### ## ##')}</Text>
     </main>
   )
 }
