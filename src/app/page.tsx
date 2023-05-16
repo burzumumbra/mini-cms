5e009c132cd032d88d589655245d7228e7d5ca15
ronald-flores-devbase <ronald@ronaldsequeira.com>
2023-05-15 21:31:46 -0600

    faking data

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,22 @@
-import { Card } from "@tremor/react";
+import { Text } from "@tremor/react";
 import Hero from "@/components/hero";
+import { faker } from '@faker-js/faker';
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-        <Hero></Hero>
+    <main className="flex min-h-screen flex-col gap-4 items-center justify-between p-24">
+        <Hero />
+
+        <div className="flex flex-row gap-2">
+          <Text className="max-w-[400px]">{faker.lorem.lines(4)}</Text>
+          <img width={200} src={faker.image.nature(200, 150, true)} alt="hero" />
+          <Text className="max-w-[200px]">{faker.lorem.lines(3)}</Text>
+        </div>
+        <div className="flex flex-row gap-2">
+          <img width={200} src={faker.image.nature(100, 150, true)} alt="hero" />
+          <img width={200} src={faker.image.nature(100, 150, true)} alt="hero" />
+          <img width={200} src={faker.image.nature(100, 150, true)} alt="hero" />
+        </div>
       </main>
   )
 }
--- src/components/hero/index.tsx
+++ src/components/hero/index.tsx
@@ -1,11 +1,12 @@
 import { Flex, Title, Text } from "@tremor/react";
+import { faker } from '@faker-js/faker';
 
 const Hero = () => {
   return (
     <Flex className="relative bg-blend-overlay bg-slate-600 h-[400px] bg-cover bg-[url('../../public/hero-bg.jpeg')]">
       <div className="flex flex-col max-w-[300px] bg-slate-200 absolute bottom-4 right-4 p-2">
-        <Title>Lorem ipsum dolor</Title>
-        <Text>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.</Text>
+        <Title>{faker.lorem.lines(1)}</Title>
+        <Text>{faker.lorem.lines(2)}</Text>
       </div>
     </Flex>
   );
